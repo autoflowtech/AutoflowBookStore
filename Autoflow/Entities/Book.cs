17df3a9f49ae18d93bcfcd47757b727b58aef1d1
Ben Abbott <B.Abbott@autoflow.ltd.uk>
2025-07-23 13:57:45 +0100

    add reviews

--- Autoflow/Entities/Book.cs
+++ Autoflow/Entities/Book.cs
@@ -10,6 +10,6 @@ public class Book
     public int Id { get; set; }
     public string Name { get; set; }
     public string Author { get; set; }
-    public string Language { get; set; }
+    public int Rating { get; set; }
     public float Price { get; init; }
 }
